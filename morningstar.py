4d4c4a1d0e6e00eb736709d3986cd7a60fa3fc3b
Anshu Pandey <anshuspandey@gmail.com>
2024-12-18 10:07:53 +0530

    morningstar package

---
+++ morningstar.py
@@ -0,0 +1,32 @@
+def greet():
+    print("Hello and welcome")
+
+def morningStar(a,b):
+    """ 
+    This function can accept two numbers and returns addition
+    Arguments:
+    a: int/float/Bool/complex
+    b: int/float/bool/complex
+
+    returns:
+    c: int/float
+
+    Example:
+    morningStar(4,5)
+    >> 9
+    """
+    c = a+b
+    return c
+
+class employee:
+    name = ""
+    salary = ""
+
+    def onboarding(self):
+        print("Welcome to the team")
+        return None
+    
+
+def anshu():
+    print("hiii")
+
